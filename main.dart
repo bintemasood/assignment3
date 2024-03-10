17f7fbfebb3bd9ac7228b6a11010c73a2d3fd55b
aliza <m16148411@gmail.com>
2024-03-10 14:10:16 +0500

    assignment3

---
+++ main.dart
@@ -0,0 +1,36 @@
+void main() {
+  //Assignment #3
+  //Q1...
+  int length = 5;
+  int breadth = 5;
+  if (length == breadth) {
+    print("It's a Square");
+  } else {
+    print("It's a Rectangle");
+  }
+
+  //Q2....
+  int personA = 20;
+  int personB = 30;
+  if (personA > personB) {
+    print("PersonA is oldest");
+    print("personB is oldest");
+  } else {
+    print("personB is oldest");
+    print("personA is youngest");
+  }
+
+  var num = 2;
+  if (num > 0) {
+    print("${num} is positive");
+  }
+    else if (num<0){
+      print("${num} is negative");
+    }else{
+      print("${num} is neither positive or negative number");
+  }
+
+  //marksheet
+  //
+  //int math = 85
+}
